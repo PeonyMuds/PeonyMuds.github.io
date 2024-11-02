dd215f4abab759760a5b49a7e710019e8d2d3f28
S. Wei <luckveethegreat@outlook.com>
2024-11-02 17:34:28 +0800

    update pact code

---
+++ scripts/loadnav.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('elements/nav.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('content').innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Error loading HTML:', error);
+        document.getElementById('content').innerHTML = '<p>Failed to load content.</p>';
+      });
+  });
