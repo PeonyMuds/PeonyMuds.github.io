c4aa05fb7b7e89dd6b50e2562950bf5ceb9a9cc3
S. Wei <luckveethegreat@outlook.com>
2024-11-02 17:46:58 +0800

    updating pact codes

--- scripts/loadfooter.js
+++ scripts/loadfooter.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('elements/footer.html')
       .then(response => response.text())
       .then(data => {
-        document.getElementById('content').innerHTML = data;
+        document.getElementById('jsfooter').innerHTML = data;
       })
       .catch(error => {
         console.error('Error loading HTML:', error);
-        document.getElementById('content').innerHTML = '<p>Failed to load content.</p>';
+        document.getElementById('jsfooter').innerHTML = '<p>Failed to load content.</p>';
       });
   });
--- scripts/loadnav.js
+++ scripts/loadnav.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('elements/nav.html')
       .then(response => response.text())
       .then(data => {
-        document.getElementById('content').innerHTML = data;
+        document.getElementById('jsnav').innerHTML = data;
       })
       .catch(error => {
         console.error('Error loading HTML:', error);
-        document.getElementById('content').innerHTML = '<p>Failed to load content.</p>';
+        document.getElementById('jasnav').innerHTML = '<p>Failed to load content.</p>';
       });
   });
