2daebf29d3f10b01e0cccd4ed6ee82cb8eabdd96
S. Wei <luckveethegreat@outlook.com>
2024-11-04 00:29:58 +0800

    updates

--- scripts/loadfooter.js
+++ scripts/loadfooter.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('elements/footer.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('jsfooter').innerHTML = data;
-      })
-      .catch(error => {
-        console.error('Error loading HTML:', error);
-        document.getElementById('jsfooter').innerHTML = '<p>Failed to load content.</p>';
-      });
-  });
+  fetch('elements/footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('jsfooter').innerHTML = data;
+      document.getElementById('jsfooter-small').innerHTML = data;
+    })
+    .catch(error => {
+      console.error('Error loading HTML:', error);
+      document.getElementById('jsfooter').innerHTML = '<p>Failed to load content.</p>';
+      document.getElementById('jsfooter-small').innerHTML = '<p>Failed to load content.</p>';
+    });
+});
