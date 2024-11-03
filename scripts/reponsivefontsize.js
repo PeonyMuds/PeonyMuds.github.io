ea42db474683a4459adb4bbf16514885c138a2fd
S. Wei <luckveethegreat@outlook.com>
2024-11-03 23:25:34 +0800

    updates

--- scripts/reponsivefontsize.js
+++ scripts/reponsivefontsize.js
@@ -6,11 +6,11 @@
             var width = docEle.clientWidth;
             if (width) {
                 var fontSize = 20 * (width / 320);
-                fontSize = Math.max(14, Math.min(fontSize, 20)); // 设置最小字体为12px，最大字体为40px
+                fontSize = Math.max(14, Math.min(fontSize, 20)); // 设置最小字体为14px，最大字体为40px
                 docEle.style.fontSize = fontSize + "px";
             }
         };
-
+  
     win.addEventListener(evt, fn, false);
     doc.addEventListener("DOMContentLoaded", fn, false);
   }(document, window));
